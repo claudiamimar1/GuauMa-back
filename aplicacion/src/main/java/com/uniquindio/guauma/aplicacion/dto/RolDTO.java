7a0daa50408d809d76a2e27f042808756238c9bf
carolina.marin <cmarinhincapie1@gmail.com>
2023-09-04 21:55:50 -0500

    Ajuste consultas usuario

--- aplicacion/src/main/java/com/uniquindio/guauma/aplicacion/dto/RolDTO.java
+++ aplicacion/src/main/java/com/uniquindio/guauma/aplicacion/dto/RolDTO.java
@@ -1,6 +1,13 @@
 
 package com.uniquindio.guauma.aplicacion.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RolDTO {
 
     private Long idRol;
